f465b111d9f14384987538de5cf0de332c3a4c51
ibuler <ibuler@qq.com>
2026-04-02 11:17:22 +0800

    perf: move to osm

--- apps/common/urls/api_urls.py
+++ apps/common/urls/api_urls.py
@@ -11,11 +11,11 @@ app_name = 'common'
 urlpatterns = [
     path('resources/cache/', api.ResourcesIDCacheApi.as_view(), name='resources-cache'),
     path('countries/', api.CountryListApi.as_view(), name='resources-cache'),
-    path('file/crypto/', api.FileCryptoApi.as_view(), name='file-crypto'),
 ]
 
 if settings.WEBHOOK_ENABLED:
     urlpatterns.append(path('webhook/', api.WebhookApi.as_view(), name='webhooks'))
 
 if settings.JDMC_ENABLED:
-    urlpatterns.append(path('jdmc/sso-token/', api.JdmcSSOTokenAPI.as_view(), name='jdmc-sso-token'))
+    from xpack.plugins.jdmc.api import JdmcSSOTokenAPI
+    urlpatterns.append(path('jdmc/sso-token/', JdmcSSOTokenAPI.as_view(), name='jdmc-sso-token'))
