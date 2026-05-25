7fc87644d6ec99eac62d9af5040421902cf8ff2e
Bai <baijiangjie@gmail.com>
2026-05-25 19:20:43 +0800

    feat: modify cert login url

--- apps/authentication/backends/cert/view_urls.py
+++ apps/authentication/backends/cert/view_urls.py
@@ -3,5 +3,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('cert/login/', views.CertLoginView.as_view(), name='cert-login')
+    path('login/', views.CertLoginView.as_view(), name='cert-login')
 ]
